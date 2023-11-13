f238714307b2599d5ca9f853de78d2857c3ed2b9
Altaf Fattah <altaf.fattah05@gmail.com>
2023-11-13 22:47:23 +0700

    Fix variable name

--- tugas_uas_andal.cpp
+++ tugas_uas_andal.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 #include <limits>
-
 using namespace std;
 
 int main()
 {
-    // Variabel input
     string ro, him;
     int fir, man, syah, oim, iim, roh;
     char boim;
 
-    // Variabel proses gaji
     int mas, baim, firman, irma, asya, maman, ihim, acim;
 
-    // Input NIM dan NAMA Programer
     string aim, hoim;
     cout << "Masukkan NAMA Programer: ";
     getline(cin, hoim);
@@ -22,7 +18,6 @@ int main()
 
     do
     {
-        // Input data karyawan
         cout << "Nama Pegawai : ";
         getline(cin, ro);
         cout << "Masukan NIP : ";
@@ -31,22 +26,18 @@ int main()
         cin >> fir;
         man = 2023 - fir;
 
-        // Pilihan jabatan
         cout << "Pilih Jabatan:\n";
         cout << "1. Staff\n2. Manager\n3. Direktur\nJabatan: ";
         cin >> syah;
 
-        // Pilihan status karyawan
         cout << "Pilih Status Karyawan:\n";
         cout << "1. Tetap\n2. Honorer\nStatus Karyawan: ";
         cin >> oim;
 
-        // Pilihan status pernikahan
         cout << "Pilih Status Pernikahan:\n";
         cout << "1. Menikah\n2. Belum Menikah\nStatus Pernikahan: ";
         cin >> iim;
 
-        // Jumlah anak (jika menikah)
         if (iim == 1)
         {
             cout << "Jumlah Anak: ";
@@ -57,7 +48,6 @@ int main()
             roh = 0;
         }
 
-        // Proses perhitungan gaji
         if (syah == 3)
         {
             mas = 7000000;
@@ -93,7 +83,6 @@ int main()
         ihim = 0.05 * mas;
         acim = maman - ihim;
 
-        // Output hasil
         cout << "\nNama Pegawai: " << ro << "\nNIP: " << him << "\nTahun Masuk: " << fir << "\nLama Kerja: " << man << " tahun\n";
         cout << "Pilih Jabatan:\n1. Staff\n2. Manager\n3. Direktur\nJabatan: " << syah << "\n";
         cout << "Pilih Status Karyawan:\n1. Tetap\n2. Honorer\nStatus Karyawan: " << oim << "\n";
@@ -124,16 +113,13 @@ int main()
         cout << "Gaji Bersih \t\t: " << acim << "\n";
         cout << "\n\n==========================================================================\n\n";
 
-        // Input untuk mengulang program
         cout << "Apakah Anda Ingin Mengulang menggunakan program ini? (Y/T): ";
         cin >> boim;
 
-        // Membersihkan buffer input
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     } while (boim == 'Y' || boim == 'y');
 
-    // Menampilkan informasi programer
     cout << "\n\n================================================================================\n";
     cout << "Programmer : \n";
     cout << "NIM \t\t\t: " << aim << endl;
